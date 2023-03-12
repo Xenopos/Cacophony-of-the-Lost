b0642d4d40631f5bf517ff2d69807ab1de708e0c
Xenopop <83357129+Xenopos@users.noreply.github.com>
2023-03-13 07:58:18 +0800

    Added Basic Animations and Movement Script
    added cinemachine

---
+++ My project/Assets/Scripts/MovementController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public Animator animator;
+    public float movementspeed = 5f;
+    public float jumpforce = 10f;
+    public Rigidbody2D rb; 
+    Vector2 movement;
+    public bool isJumping;
+
+    void Start() 
+    {
+        
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("speed", movement.sqrMagnitude);
+ 
+        //LastInputX lets shi to face on the last direction on idle
+
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") ==  -1 )
+        {
+            animator.SetFloat("LastInputX", Input.GetAxisRaw("Horizontal"));
+        }
+        if(Input.GetButtonDown("Jump"))
+        {
+         rb.AddForce(new Vector2(rb.velocity.x, jumpforce));
+        }
+ 
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementspeed * Time.fixedDeltaTime);
+    }
+}
