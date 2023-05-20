22a398783cb3244d203846104c0d0e767411e136
Xenopop <83357129+Xenopos@users.noreply.github.com>
2023-05-20 14:26:58 +0800

    CUTSCENE SA LIKOD doNE

---
+++ My project/Assets/Sanity_Cutscene.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Player;
+using UnityEngine.SceneManagement;
+
+public class Sanity_Cutscene : MonoBehaviour
+{
+    public VideoPlayer LowSan;
+    public VideoPlayer HighSan;
+
+    void Start()
+    {
+    LowSan.enabled = false;
+    HighSan.enabled = false;
+        end();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void end()
+    {
+            if(PlayerStateManager.sanityLevel <= 50)
+            {
+
+                StartCoroutine(WaitforVideoEnd3());
+                LowSan.enabled = true;
+            }
+
+            else if(PlayerStateManager.sanityLevel > 50)
+            {
+
+                StartCoroutine(WaitforVideoEnd1());
+                HighSan.enabled = true;
+            }
+
+    }
+
+    IEnumerator WaitforVideoEnd1()
+    {
+        float videolength = 14f;
+        yield return new WaitForSeconds(videolength);
+                           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+        IEnumerator WaitforVideoEnd3()
+    {
+        float videolengthr = 44f;
+        yield return new WaitForSeconds(videolengthr);
+                           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
